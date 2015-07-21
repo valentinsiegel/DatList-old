0db81ba9702a3705525eadd01c6881dfc84ee7ed
Val <dzdzdzd>
2015-07-21 19:00:28 +0200

    Initial commit

---
+++ app/src/main/java/fr/siegel/datlist/MainActivity.java
@@ -0,0 +1,26 @@
+package fr.siegel.datlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import datlist.siegel.fr.datlist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+
+}
